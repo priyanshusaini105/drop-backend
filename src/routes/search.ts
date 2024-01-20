53f8c9c67db3308a8770585612c6dd7a1b9c0e5d
Piyanshu Saini <priyanshusaininew@gmail.com>
2024-01-20 12:20:52 +0530

    thime ]]]

--- src/routes/search.ts
+++ src/routes/search.ts
@@ -11,6 +11,23 @@ searchRouter.get("/autocomplete/:id", async (req, res) => {
     res.status(200).json({data:data[1]}); 
 })
 
+searchRouter.get("/search/:q", async (req, res) => {
+    const q = req.params.q
+    const url = new URL('https://www.googleapis.com/customsearch/v1');
+    url.searchParams.append('key', 'AIzaSyCArdW91vTSFTn_VY9kGWB33MAclJ2D1wk');
+    url.searchParams.append('cx', '07ef0d9b7e3454b6b');
+    url.searchParams.append('q', q??'');
+
+    const response = await fetch(url.toString());
+    const result = await response.json().items[0].link;
+
+    res.status(200).json({data:`<iframe id="price_frame" src="https://pricehistoryapp.com/embed/${
+        result.split("/")[4]
+      }" width="100%" height="500" frameborder="0" allowtransparency="true" scrolling="no"></iframe>`
+    });
+})
+
+
 
 
 export default searchRouter;
